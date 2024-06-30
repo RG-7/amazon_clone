cb2fec5022a6553d2300d48a4e9b8a0eb3be7649
Ratn Govindam <rg.ratn@gmail.com>
2024-07-01 00:14:03 +0530

    Menu Page Ui Done!

--- lib/view/user/menu/menu_screen.dart
+++ lib/view/user/menu/menu_screen.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_clone/constants/common_functions.dart';
+import 'package:amazon_clone/utils/colors.dart';
+import 'package:amazon_clone/view/user/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class MenuScreen extends StatefulWidget {
@@ -10,6 +13,92 @@ class MenuScreen extends StatefulWidget {
 class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final textTheme = Theme.of(context).textTheme;
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size(width * 1, height * 0.08),
+          child: const HomePageAppBar(),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            width: width,
+            padding: EdgeInsets.symmetric(
+              horizontal: width * 0.03,
+              vertical: height * 0.02,
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: appBarGradientColor,
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+            child: Column(
+              children: [
+                GridView.builder(
+                  itemCount: 18,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 0.7,
+                  ),
+                  shrinkWrap: true,
+                  physics: const PageScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                              'assets/images/menu_pics/$index.png',
+                            ),
+                            fit: BoxFit.fill),
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: greyShade3,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                CommonFunctions.blankSpace(height * 0.02, 0),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const PageScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: height * 0.005),
+                      padding: EdgeInsets.symmetric(
+                          vertical: height * 0.005, horizontal: width * 0.03),
+                      height: height * 0.06,
+                      width: width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: white,
+                        border: Border.all(color: teal),
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            index == 0 ? 'Settings' : 'Customer Service',
+                            style: textTheme.bodyMedium,
+                          ),
+                          const Spacer(),
+                          Icon(Icons.chevron_right_rounded,color: black,)
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: 2,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
